12a88156d51eace4db73134ace3390dda968c2e6
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2022-03-04 15:36:56 +0200

    Add morgan + add error handling if conditions

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const axios = require("axios");
+const helmet = require("helmet");
 const app = express();
+const morgan = require("morgan");
+const _ = require("lodash");
 const apiEndPoint = "https://www.google.com/recaptcha/api/siteverify";
 const gCaptchaSecretKey = "6LeNloweAAAAAK_ngbhSQtPj8_RmnvQBfyx29AB8";
 const axiosConfig = {
@@ -11,6 +14,11 @@ const axiosConfig = {
   "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
 };
 app.use(express.json());
+app.use(helmet());
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("Morgan Enabled");
+}
 // app.use(function (req, res, next) {
 //   console.log("Amr");
 // });
@@ -19,6 +27,17 @@ app.get("/api/me", (req, res) => {
   res.status(200).send("Hello world ");
 });
 app.post("/api/verify", async (req, res) => {
+  if (_.isEmpty(req.body)) {
+    res.status(400).send("The request is empty, please add user response");
+    return;
+  }
+  if (
+    req.body["userResponse"] == undefined ||
+    _.isEmpty(req.body["userResponse"])
+  ) {
+    res.status(400).send("User response not found");
+    return;
+  }
   const { userResponse } = req.body;
   let params = { secret: gCaptchaSecretKey, response: userResponse };
   try {
@@ -31,8 +50,7 @@ app.post("/api/verify", async (req, res) => {
   } catch (error) {
     console.log("Errors: ", error);
   }
-  const status = { data };
-  res.status(200).send(status);
+  res.status(200).send(data);
 });
 
 app.listen(3001, () => console.log("Listening on port 3001..."));
